3bada2ab6b86fdd46c7f4ea83c81f2123ccf3354
xianchaoxing <xbob75969@gmail.com>
2022-09-09 13:54:24 +0800

    algorithm: backtracking 2nd week
    Signed-off-by: xianchaoxing <xbob75969@gmail.com>

--- algorithm/backtracking_test.go
+++ algorithm/backtracking_test.go
@@ -37,3 +37,24 @@ func TestCombinationSum2(t *testing.T) {
 func TestPartition(t *testing.T) {
 	fmt.Println(partition("aab"))
 }
+
+func TestRestoreIpAddresses(t *testing.T) {
+	// fmt.Println(restoreIpAddresses("0000"))
+	fmt.Println(restoreIpAddresses("25525511135"))
+}
+
+func TestFindSubsequence(t *testing.T) {
+	fmt.Println(findSubsequences([]int{1, 3, 2, 2}))
+	fmt.Println(findSubsequences([]int{4, 6, 7, 7}))
+	fmt.Println(findSubsequences([]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 1, 1, 1, 1}))
+	// output:
+	// [[4,6],[4,6,7],[4,6,7,7],[4,7],[4,7,7],[6,7],[6,7,7],[7,7]]
+}
+
+func TestPermute(t *testing.T) {
+	fmt.Println(permute([]int{1, 2, 3}))
+}
+
+func TestPermuteUnique(t *testing.T) {
+	fmt.Println(permuteUnique([]int{1, 2, 1}))
+}
